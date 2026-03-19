d399f78ae98b22baa820c74e76f588053460c9b8
katuoaikouka <yosinaka081@gmail.com>
2026-03-19 13:13:20 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+import Server from 'bare-server-node';
+import http from 'http';
+import nodeStatic from 'node-static';
+import path from 'path';
+
+const bare = new Server('/bare/', '');
+const serve = new nodeStatic.Server('static/');
+
+const server = http.createServer();
+
+server.on('request', (request, response) => {
+    if (bare.route_request(request, response)) return;
+    serve.serve(request, response);
+});
+
+server.on('upgrade', (req, socket, head) => {
+    if (bare.route_upgrade(req, socket, head)) return;
+    socket.end();
+});
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
